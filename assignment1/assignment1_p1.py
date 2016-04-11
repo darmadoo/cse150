dd75da6deed9b895909a293365a24017943adbae
darmadoo <markdarmadi@gmail.com>
2016-04-11 16:19:28 -0700

    Populated prime list

--- assignment1/assignment1_p1.py
+++ assignment1/assignment1_p1.py
@@ -1,6 +1,20 @@
 __author__ = 'mdarmadi@ucsd.edu, A11410141, '
 import sys
 
+allPrime = []
+
+def isprime(n):
+    # if n < 2:
+    #     return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for x in range(3, int(n**0.5)+1, 2):
+        if n % x == 0:
+            return False
+    return True
+
 def getPossibleActions(currentPrime):
 	# This method would return the list of prime
 	# numbers reachable from current prime .
@@ -15,11 +29,17 @@ def getPath(startingPrime, finalPrime):
 	return getPath
 
 def main():
+	allNum = []
+	for x in range(2, 1000001):
+		allNum.append(x)
+
+	for i in allNum:
+		if isprime(i):
+			allPrime.append(i)
+
 	primes = str(sys.stdin.readline()).split()
 	print(getPath(primes[0], primes[1]))
 
 
 if __name__ == '__main__':
 	main()
-
-#change by pycharm
