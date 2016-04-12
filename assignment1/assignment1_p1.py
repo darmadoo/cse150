b598a5974b10a56ede63118b3a0e3dba1435ecff
Vania Chandra <vanvania.ia@hotmail.com>
2016-04-12 10:59:17 -0700

    Fixed getPossibleActions; still include Frontier and closedList primes

--- assignment1/assignment1_p1.py
+++ assignment1/assignment1_p1.py
@@ -2,12 +2,10 @@ __author__ = 'mdarmadi@ucsd.edu, A11410141, '
 import sys
 
 
-#allPrime is going to be a dictionary showing whether a number is prime
-allPrime = {}
-#closedList is going to e a dictionary showing whether a prime number has been visited or not 
-closedList = {}	
+allPrime = {} #allPrime is going to be a dictionary showing whether a number is prime
+closedList = {} #closedList is going to e a dictionary showing whether a prime number has been visited or not
 
-def isprime(n):
+def isPrime(n):
     # if n < 2:
     #     return False
     if n == 2:
@@ -19,69 +17,75 @@ def isprime(n):
             return False
     return True
 
+# This method would return the list of prime
+# numbers reachable from current prime .
+# Note - this should not include the prime numbers
+# which have already been processed, either in the
+# frontier or in the closedlist.
+
 def getPossibleActions(currentPrime):
-	# This method would return the list of prime
-	# numbers reachable from current prime .
-	# Note - this should not include the prime numbers
-	# which have already been processed, either in the
-	# frontier or in the closedlist.
-	
-	length = 0
-	listOfPrimes = []
-	
-	currentStr = str(currentPrime)
-	currentList = list(currentStr)
-	length = len(currentList)   #this is the length of the number
-	
-	#check every possible combination
-	for i in range(0, length):
-		curChar = currentList[i]
-		for j in range(0,10):
-			j = str(j)
-			if(j == curChar):
-				continue
-			currentList[i] = j
-			newStr = ''.join(currentList)
-			newInt = int(newStr)            #might not need to convert to int if dictionary uses str for key
-			
-			#see if curInt is a prime
+    listOfPrimes = []
+
+    currentStr = str(currentPrime)
+    currentList = list(currentStr)
+    length = len(currentList)   # this is the digit of the number
+
+    # check every possible combination
+    for i in range(0, length):
+        curChar = currentList[i]
+        for j in range(0,10): # possible digit replacement
+            j = str(j)
+
+            if(j == curChar): # to avoid repetition
+                continue
+
+            if (j == '0' and i == 0):    # to avoid producing leading 0
+                continue
+
+            currentList[i] = j          # replace the digit
+            newStr = ''.join(currentList)
+            newInt = int(newStr)            # might not need to convert to int if dictionary uses str for key
+
+            #see if curInt is a prime
 # 			if(allPrime[newInt] and not closedList[newInt]):
 # 				listOfPrimes.append(newInt)
-			print(newStr)
-						
-			# return currentList to original char
-			currentList[i] = curChar
-	
-	return listOfPrimes
+            if (isPrime(newInt)):
+                listOfPrimes.append(newInt)
+
+            currentList[i] = curChar # return currentList to original char
+
+    print("possible actions: ")
+    print(listOfPrimes)
+
+    return listOfPrimes
 
 
 def getPath(startingPrime, finalPrime):
-	# your code here
-	return getPath
+    # your code here
+    return getPath
 
 def main():
-	allNum = []
-	for x in range(2, 1000001):
-		allNum.append(x)
+    allNum = []
+    for x in range(2, 1000001):
+        allNum.append(x)
 
-	for i in allNum:
-		if isprime(i):
+    for i in allNum:
+        if isPrime(i):
+            allPrime[i] = True
+        else:
+            allPrime[i] = False
 
-			allPrime[i] = True
-		else:
-			allPrime[i] = False
-
-	# Replacing number in int ex. from 103 -> 203
-	j = 103
-	numj = str(j)
-	listj = list(numj)
-	listj[0] = '2'
-	numj = ''.join(listj)
+    # Replacing number in int ex. from 103 -> 203
+    j = 103
+    numj = str(j)
+    listj = list(numj)
+    listj[0] = '2'
+    numj = ''.join(listj)
 
 
-	primes = str(sys.stdin.readline()).split()
-	getPossibleActions(113)
+    primes = str(sys.stdin.readline()).split()
+    getPossibleActions(89)
 
 
 if __name__ == '__main__':
-	main()
+    main()
