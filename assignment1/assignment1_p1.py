977aaa3fbd795f82562973163495e41af1b03394
Hansen <hansen_t_d@hotmail.co.uk>
2016-04-11 19:12:50 -0700

    getPossibleAction half implemented using for loops

--- assignment1/assignment1_p1.py
+++ assignment1/assignment1_p1.py
@@ -1,7 +1,11 @@
 __author__ = 'mdarmadi@ucsd.edu, A11410141, '
 import sys
 
+#allPrime is going to be a dictionary showing whether a number is prime
 allPrime = []
+
+#closedList is going to e a dictionary showing whether a prime number has been visited or not 
+closedList = {}	
 
 def isprime(n):
     # if n < 2:
@@ -21,6 +25,33 @@ def getPossibleActions(currentPrime):
 	# Note - this should not include the prime numbers
 	# which have already been processed, either in the
 	# frontier or in the closedlist.
+	
+	length = 0
+	listOfPrimes = []
+	
+	currentStr = str(currentPrime)
+	currentList = list(currentStr)
+	length = len(currentList)   #this is the length of the number
+	
+	#check every possible combination
+	for i in range(0, length):
+		curChar = currentList[i]
+		for j in range(0,10):
+			j = str(j)
+			if(j == curChar):
+				continue
+			currentList[i] = j
+			newStr = ''.join(currentList)
+			newInt = int(newStr)            #might not need to convert to int if dictionary uses str for key
+			
+			#see if curInt is a prime
+# 			if(allPrime[newInt] and not closedList[newInt]):
+# 				listOfPrimes.append(newInt)
+			print(newStr)
+						
+			# return currentList to original char
+			currentList[i] = curChar
+	
 	return listOfPrimes
 
 
@@ -36,9 +67,9 @@ def main():
 	for i in allNum:
 		if isprime(i):
 			allPrime.append(i)
-
+			allPrime['key'] = value
 	primes = str(sys.stdin.readline()).split()
-	print(getPath(primes[0], primes[1]))
+	getPossibleActions(113)
 
 
 if __name__ == '__main__':
