25eebc7e94daaeba6bc90baa6e23adf4777b4d69
Hansen <hansen_t_d@hotmail.co.uk>
2016-04-12 14:07:37 -0700

    Implemented limited DFS, have not implemented output

--- assignment1/assignment1_p2.py
+++ assignment1/assignment1_p2.py
@@ -1,17 +1,97 @@
 # _ _author_ _ = 'mdarmadi@ucsd.edu, A11410141, '
 import sys
+from Queue import LifoQueue
+
+closedList = {} # closedList is going to be a dictionary showing the parent of number
+
+def isPrime(n):
+    # if n < 2:
+    #     return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for x in range(3, int(n**0.5)+1, 2):
+        if n % x == 0:
+            return False
+    return True
 
 def getPossibleActions(currentPrime):
-	# This method would return the list of prime
-	# numbers reachable from current prime .
-	# Note - this should not include the prime numbers
-	# which have already been processed, either in the
-	# frontier or in the closedlist.
-	return listOfPrimes
+    listOfPrimes = []
+
+    currentStr = str(currentPrime)
+    currentList = list(currentStr)
+    length = len(currentList)   # this is the digit of the number
+
+    # check every possible combination
+    for i in range(0, length):
+        curChar = currentList[i]
+        for j in range(0,10): # possible digit replacement
+            j = str(j)
+
+            if(j == curChar): # to avoid repetition
+                continue
+
+            if (j == '0' and i == 0):    # to avoid producing leading 0
+                continue
+
+            currentList[i] = j          # replace the digit
+            newStr = ''.join(currentList)
+            newInt = int(newStr)            # might not need to convert to int if dictionary uses str for key
+
+            #see if curInt is a prime
+# 			if(allPrime[newInt] and not closedList[newInt]):
+# 				listOfPrimes.append(newInt)
+
+            # check if new integer is prime and not in closedList already
+            if (isPrime(newInt)):
+                if (not newInt in closedList):
+                    listOfPrimes.append(newInt)
+
+            currentList[i] = curChar # return currentList to original char
+
+    print("possible actions for " + str(currentPrime) + " is: " + str(listOfPrimes))
+    return listOfPrimes
 
 
 def getPath(startingPrime, finalPrime):
 	# your code here
+	#depth limit is 5
+	
+	#declare currentPrime
+	currentPrime
+	#declare stack
+	stack = LifoQueue()
+	
+	#push <startingPrime (currentPrime), 0 (depth)> into the stack
+	stack.put((startingPrime , 0))
+	
+	#while stack is not empty 
+	while(not stack.empty()):
+		#pop a from stack
+		a = stack.get()
+		#if a.currentPrime == finalPrime
+		if(a[0] == finalPrime):
+			break
+		#else if a.depth >= 5
+		elif(a[1] >= 5):
+			continue
+		
+		#find all neighbor of currentPrime
+		neighbor = getPossibleActions(a[0])
+		
+		for i in neighbor:
+			#set the parent of the neighbor to currentPrime
+			closedList[neighbor[i]] = a[0]
+			#push all neighbor as <neighbor,a.depth + 1> into the stack
+			stack.put((neighbor[i],a[1] + 1))
+		
+	
+	#if(currentPRime != finalPrime)
+	if(a[0] != finalPrime):
+		#unsolvable
+		return -1
+	#print the found path
 	return getPath
 
 def main():
