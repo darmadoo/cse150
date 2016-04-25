798066cbfba73b85097ab5b432005c3b454f75be
darmadoo <markdarmadi@gmail.com>
2016-04-24 20:53:53 -0700

    Passed 2/3 test cases

--- assignment2/solutions/p1_minimax_player.py
+++ assignment2/solutions/p1_minimax_player.py
@@ -3,6 +3,7 @@ __author__ = 'Dan'
 __email__ = 'daz040@eng.ucsd.edu'
 
 from assignment2 import Player, State, Action
+import sys
 
 class MinimaxPlayer(Player):
     def __init__(self):
@@ -15,4 +16,46 @@ class MinimaxPlayer(Player):
         :param state: State, the current state of the board.
         :return: Action, the next move
         """
-        raise NotImplementedError("Need to implement this method")
+        best_act = None
+        best_v = -sys.maxint
+
+        for actions in state.actions():
+            v = self.minVal(state.result(actions))
+            if best_v < v:
+                best_v = v
+                best_act = actions
+
+        print "FINAL"
+        print best_act
+        print best_v
+        return best_act
+
+    def minVal(self, state):
+        if state.is_win():
+            print "won"
+            return state.utility(self)
+
+        v = sys.maxint
+
+        if not state.actions():
+            state.result(None)
+        else:
+            for a in state.actions():
+                min(self.maxVal(state.result(a)), v)
+
+        return v
+
+    def maxVal(self, state):
+        if state.is_win():
+            print "won"
+            return state.utility(self)
+
+        v = -sys.maxint
+
+        if not state.actions():
+            state.result(None)
+        else:
+            for a in state.actions():
+                max(self.minVal(state.result(a)), v)
+
+        return v
