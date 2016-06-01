23e66ba7c37a26fd112bbb952819b9e0cced8540
darmadoo <markdarmadi@gmail.com>
2016-05-31 18:34:09 -0700

    First Try

--- assignment4/BayesianNetwork.py
+++ assignment4/BayesianNetwork.py
@@ -1,51 +1,52 @@
 #!/usr/bin/env python
 """ generated source for module BayesianNetwork """
 from Assignment4 import *
-# 
+import random
+#
 #  * A bayesian network
 #  * @author Panqu
-#  
+#
 class BayesianNetwork(object):
     """ generated source for class BayesianNetwork """
-    # 
+    #
     #     * Mapping of random variables to nodes in the network
-    #     
+    #
     varMap = None
 
-    # 
+    #
     #     * Edges in this network
-    #     
+    #
     edges = None
 
-    # 
+    #
     #     * Nodes in the network with no parents
-    #     
+    #
     rootNodes = None
 
-    # 
+    #
     #     * Default constructor initializes empty network
-    #     
+    #
     def __init__(self):
         """ generated source for method __init__ """
         self.varMap = {}
         self.edges = []
         self.rootNodes = []
 
-    # 
+    #
     #     * Add a random variable to this network
     #     * @param variable Variable to add
-    #     
+    #
     def addVariable(self, variable):
         """ generated source for method addVariable """
         node = Node(variable)
         self.varMap[variable]=node
         self.rootNodes.append(node)
 
-    # 
+    #
     #     * Add a new edge between two random variables already in this network
     #     * @param cause Parent/source node
     #     * @param effect Child/destination node
-    #     
+    #
     def addEdge(self, cause, effect):
         """ generated source for method addEdge """
         source = self.varMap.get(cause)
@@ -56,7 +57,7 @@ class BayesianNetwork(object):
         if dest in self.rootNodes:
             self.rootNodes.remove(dest)
 
-    # 
+    #
     #     * Sets the CPT variable in the bayesian network (probability of
     #     * this variable given its parents)
     #     * @param variable Variable whose CPT we are setting
@@ -64,7 +65,7 @@ class BayesianNetwork(object):
     #       Write out the cpt by hand, with each column representing one of the parents (in alphabetical order).
     #       Then assign these parent variables true/false based on the following order: ...tt, ...tf, ...ft, ...ff.
     #       The assignments in the right most column, P(V=true|P1,P2,...), will be the values you should pass in as probabilities here.
-    #     
+    #
     def setProbabilities(self, variable, probabilities):
         """ generated source for method setProbabilities """
         probList = []
@@ -72,39 +73,61 @@ class BayesianNetwork(object):
             probList.append(probability)
         self.varMap.get(variable).setProbabilities(probList)
 
-    # 
+    #
     #     * Returns an estimate of P(queryVal=true|givenVars) using rejection sampling
     #     * @param queryVar Query variable in probability query
     #     * @param givenVars A list of assignments to variables that represent our given evidence variables
     #     * @param numSamples Number of rejection samples to perform
-    #     
+    #
     def performRejectionSampling(self, queryVar, givenVars, numSamples):
         """ generated source for method performRejectionSampling """
         #  TODO
+        n = {}
+        # print queryVar
+        # for i in range(1, numSamples):
+        x = self.priorSampling()
+
+
         return 0
 
-    # 
+    #
     #     * Returns an estimate of P(queryVal=true|givenVars) using weighted sampling
     #     * @param queryVar Query variable in probability query
     #     * @param givenVars A list of assignments to variables that represent our given evidence variables
     #     * @param numSamples Number of weighted samples to perform
-    #     
+    #
     def performWeightedSampling(self, queryVar, givenVars, numSamples):
         """ generated source for method performWeightedSampling """
         #  TODO
-        # test
-
         return 0
 
-    # 
+    #
     #     * Returns an estimate of P(queryVal=true|givenVars) using Gibbs sampling
     #     * @param queryVar Query variable in probability query
     #     * @param givenVars A list of assignments to variables that represent our given evidence variables
     #     * @param numTrials Number of Gibbs trials to perform, where a single trial consists of assignments to ALL
     #       non-evidence variables (ie. not a single state change, but a state change of all non-evidence variables)
-    #     
+    #
     def performGibbsSampling(self, queryVar, givenVars, numTrials):
         """ generated source for method performGibbsSampling """
         #  TODO
         return 0
 
+    def priorSampling(self):
+        # an event with n elements
+        x = [0] * len(self.varMap)
+
+        new = sorted(self.varMap)
+
+        for i in new:
+            # Generated Random numbers
+            rand = random.uniform(0, 1)
+
+            if self.varMap.get(i).getParents():
+                # sample them
+                print "ITS TRUE"
+            else:
+                print "False"
+                # print self.varMap.get(i).getProbability(BLAH, BLAH)
+
+        return 0
